c57efad7a5df01da1af0a8488e7ab925e11deabf
c477 <c477@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2007-04-22 13:41:22 +0000

    update team names

--- trunk/FreeTrain/plugins/org.kohsuke.freetrain.soccerStadium/src/OpponentTeam.cs
+++ trunk/FreeTrain/plugins/org.kohsuke.freetrain.soccerStadium/src/OpponentTeam.cs
@@ -19,28 +19,28 @@ namespace freetrain.world.soccerstadium
 		}
 
 		public static readonly OpponentTeam[] OPPONENTS = new OpponentTeam[]{
-			new OpponentTeam("Sagan Tosaku",0),
-			new OpponentTeam("Hollyhock Santo",10),
-			new OpponentTeam("Shohoku Bellmare",20),
-			new OpponentTeam("Consadole Iyahoro",30),
-			new OpponentTeam("Vissel Sakagibe",40),
-			new OpponentTeam("Jubilo Iwakuchi",50),
-			new OpponentTeam("Katori Antlers",60),
-			new OpponentTeam("Tokyo Verdi",70),
-			new OpponentTeam("Kijira Reysol",80),
-			new OpponentTeam("Urawa Blues",90),
-			new OpponentTeam("Shimizu Espirits",100)
-			//! new OpponentTeam("サガン鳥酢",0),
-			//! new OpponentTeam("ホーリーホック三戸",10),
-			//! new OpponentTeam("湘北ベルマーレ",20),
-			//! new OpponentTeam("コンサドーレ礼幌",30),
-			//! new OpponentTeam("ヴィッセル榊戸",40),
-			//! new OpponentTeam("ジュピロ磐口",50),
-			//! new OpponentTeam("鹿鳥アントラーズ",60),
-			//! new OpponentTeam("束京ベルディ",70),
-			//! new OpponentTeam("木白レイソル",80),
-			//! new OpponentTeam("浦和ブルース",90),
-			//! new OpponentTeam("清水エスプリズ",100)
+			new OpponentTeam("JefYouKnighthead Chiha",0),
+			new OpponentTeam("Sunflash Hirojima",10),
+			new OpponentTeam("Yokobama-F Marinades",20),
+			new OpponentTeam("Nakoya Grandpas-eighty",30),
+			new OpponentTeam("Kajima Untollders",40),
+			new OpponentTeam("Oita Trineat",50),
+			new OpponentTeam("Shovel Iwada",60),
+			new OpponentTeam("Shimitsu Essplus",70),
+			new OpponentTeam("Nanba Ozaka",80),
+			new OpponentTeam("Kawazaki Fractale",90),
+			new OpponentTeam("Urawa Mets",100)
+			//! new OpponentTeam("ジェフユーナイトヘッド千葉",0),
+			//! new OpponentTeam("サンフラッシュ広島",10),
+			//! new OpponentTeam("横浜Ｆマリネーズ",20),
+			//! new OpponentTeam("名古屋グランパズエイティ",30),
+			//! new OpponentTeam("鹿島アントールダーズ",40),
+			//! new OpponentTeam("大分トリニート",50),
+			//! new OpponentTeam("ショベル磐田",60),
+			//! new OpponentTeam("清水エスプラス",70),
+			//! new OpponentTeam("ナンバ大阪",80),
+			//! new OpponentTeam("川崎フラクターレ",90),
+			//! new OpponentTeam("浦和メッツ",100)
 		};
 
 
