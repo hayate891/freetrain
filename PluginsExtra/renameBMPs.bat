d96d90eb3e375f9b95f932fb97fda6f7e3faf34c
stevenhoefel <stevenhoefel@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2007-05-07 09:16:12 +0000

    Bitmap renamer and quick change.

---
+++ PluginsExtra/renameBMPs.bat
@@ -0,0 +1,4 @@
+@rename .\jp.dion.kei_s.freetrain.train.TAKUROU\青い森701a.bmp .\jp.dion.kei_s.freetrain.train.TAKUROU\aoimori701a.bmp
+@rename .\jp.dion.kei_s.freetrain.train.TAKUROU\青い森701b.bmp .\jp.dion.kei_s.freetrain.train.TAKUROU\aoimori701b.bmp
+@rename .\FreeTrain.structure.piugin.6.shino\3x3buil　001.bmp .\FreeTrain.structure.piugin.6.shino\3x3buil 001.bmp
+@rename .\FreeTrain.structure.piugin.6.shino\3x3buil　001.bmp .\FreeTrain.structure.piugin.6.shino\3x3buil 002.bmp
