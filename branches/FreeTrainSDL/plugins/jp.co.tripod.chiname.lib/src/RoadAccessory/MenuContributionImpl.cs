3ffd7d568695a74b30df9fadad587a1394127c6f
jendave <jendave@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2008-03-13 15:21:23 +0000

    cleanup roadaccessory

--- branches/FreeTrainSDL/plugins/jp.co.tripod.chiname.lib/src/RoadAccessory/MenuContributionImpl.cs
+++ branches/FreeTrainSDL/plugins/jp.co.tripod.chiname.lib/src/RoadAccessory/MenuContributionImpl.cs
@@ -1,3 +1,23 @@
+#region LICENSE
+/*
+ * Copyright (C) 2004 - 2007 David Hudson (jendave@yahoo.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#endregion LICENSE
+
 using System;
 using System.Xml;
 using System.Windows.Forms;
@@ -6,25 +26,33 @@ using freetrain.contributions.others;
 
 namespace freetrain.world.road.accessory
 {
-	/// <summary>
-	/// Adds "accessory" menu to the main window
-	/// </summary>
-	public class MenuContributionImpl : MenuContribution
-	{
-		public MenuContributionImpl( XmlElement e ) : base(e) {}
+    /// <summary>
+    /// Adds "accessory" menu to the main window
+    /// </summary>
+    [CLSCompliant(false)]
+    public class MenuContributionImpl : MenuContribution
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        public MenuContributionImpl(XmlElement e) : base(e) { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="containerMenu"></param>
+        public override void mergeMenu(MainMenu containerMenu)
+        {
+            MenuItem item = new MenuItem("&Accessories...");
+          
+            item.Click += new System.EventHandler(onClick);
 
-		public override void mergeMenu( MainMenu containerMenu ) 
-		{
-			MenuItem item = new MenuItem("&Accessories...");
-			//! MenuItem item = new MenuItem("アクセサリ(&A)...");
-			item.Click += new System.EventHandler(onClick);
+            containerMenu.MenuItems[3].MenuItems.Add(item);
+        }
 
-			containerMenu.MenuItems[3].MenuItems.Add(item);
-		}
-
-		private void onClick( object sender, EventArgs e ) 
-		{
-			new ControllerForm().Show();
-		}
-	}
+        private void onClick(object sender, EventArgs e)
+        {
+            new ControllerForm().Show();
+        }
+    }
 }
--- branches/FreeTrainSDL/plugins/org.kohsuke.freetrain.land.477/VinylHouseBuilder.cs
+++ branches/FreeTrainSDL/plugins/org.kohsuke.freetrain.land.477/VinylHouseBuilder.cs
@@ -21,7 +21,6 @@
 using System;
 using System.Drawing;
 using System.Runtime.Serialization;
-using System.Windows.Forms;
 using System.Xml;
 using SDL.net;
 using freetrain.contributions.land;
