d24730d940d841a2b337b6229ffc1f1af32e12b4
jendave <jendave@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2008-03-14 10:03:58 +0000

    remove more win32 specific stuff. App shjould not use registry. It should keep settings in a config/setting file

--- branches/FreeTrainSDL/core/framework/AboutDialog.cs
+++ branches/FreeTrainSDL/core/framework/AboutDialog.cs
@@ -101,7 +101,7 @@ namespace freetrain.framework
         /// </summary>
         private void InitializeComponent()
         {
-            System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(AboutDialog));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutDialog));
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.label3 = new System.Windows.Forms.Label();
             this.okButton = new System.Windows.Forms.Button();
@@ -110,108 +110,102 @@ namespace freetrain.framework
             this.displayMode = new System.Windows.Forms.Label();
             this.browser = new System.Windows.Forms.WebBrowser();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.browser)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar
             // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right);
-            this.progressBar.Location = new System.Drawing.Point(8, 167);
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(8, 162);
             this.progressBar.Maximum = 10000;
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(320, 16);
+            this.progressBar.Size = new System.Drawing.Size(320, 17);
             this.progressBar.TabIndex = 3;
             this.progressBar.Value = 30;
             // 
             // label3
             // 
-            this.label3.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left);
-            this.label3.Location = new System.Drawing.Point(8, 151);
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.Location = new System.Drawing.Point(8, 145);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.Size = new System.Drawing.Size(96, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Available VRAM:";
-            //! this.label3.Text = "VRAM空き容量：";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // okButton
             // 
-            this.okButton.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right);
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.okButton.Location = new System.Drawing.Point(248, 191);
+            this.okButton.Location = new System.Drawing.Point(248, 188);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(80, 24);
+            this.okButton.Size = new System.Drawing.Size(80, 26);
             this.okButton.TabIndex = 5;
             this.okButton.Text = "&OK";
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // size
             // 
-            this.size.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left);
-            this.size.Location = new System.Drawing.Point(104, 151);
+            this.size.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.size.Location = new System.Drawing.Point(104, 145);
             this.size.Name = "size";
-            this.size.Size = new System.Drawing.Size(152, 16);
+            this.size.Size = new System.Drawing.Size(152, 17);
             this.size.TabIndex = 6;
             this.size.Text = "100KB/64MB";
             this.size.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label4
             // 
-            this.label4.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left);
-            this.label4.Location = new System.Drawing.Point(8, 135);
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.Location = new System.Drawing.Point(8, 127);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.Size = new System.Drawing.Size(96, 18);
             this.label4.TabIndex = 7;
             this.label4.Text = "Display mode:";
-            //! this.label4.Text = "画面モード：";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // displayMode
             // 
-            this.displayMode.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left);
-            this.displayMode.Location = new System.Drawing.Point(104, 135);
+            this.displayMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.displayMode.Location = new System.Drawing.Point(104, 127);
             this.displayMode.Name = "displayMode";
-            this.displayMode.Size = new System.Drawing.Size(144, 16);
+            this.displayMode.Size = new System.Drawing.Size(144, 18);
             this.displayMode.TabIndex = 8;
             this.displayMode.Text = "---";
             this.displayMode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // browser
             // 
-            //this.browser.Container = this;
             this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.browser.Enabled = true;
-            //this.browser.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("browser.OcxState")));
-            this.browser.Size = new System.Drawing.Size(320, 192);
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(320, 111);
             this.browser.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right);
-            this.panel1.Controls.AddRange(new System.Windows.Forms.Control[] {
-																				 this.browser});
-            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.browser);
+            this.panel1.Location = new System.Drawing.Point(8, 9);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(320, 120);
+            this.panel1.Size = new System.Drawing.Size(320, 111);
             this.panel1.TabIndex = 9;
             // 
             // AboutDialog
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
             this.ClientSize = new System.Drawing.Size(338, 223);
-            this.Controls.AddRange(new System.Windows.Forms.Control[] {
-																		  this.panel1,
-																		  this.displayMode,
-																		  this.label4,
-																		  this.size,
-																		  this.okButton,
-																		  this.label3,
-																		  this.progressBar});
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.displayMode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -219,8 +213,6 @@ namespace freetrain.framework
             this.Name = "AboutDialog";
             this.ShowInTaskbar = false;
             this.Text = "About FreeTrain";
-            //! this.Text = "FreeTrainについて";
-            ((System.ComponentModel.ISupportInitialize)(this.browser)).EndInit();
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
