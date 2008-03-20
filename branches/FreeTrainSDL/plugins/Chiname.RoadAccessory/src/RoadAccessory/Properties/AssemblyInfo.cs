450453be9a0b99aff23d284f69a786a9aeb974f3
jendave <jendave@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2008-03-20 07:42:35 +0000

    rename plugins

---
+++ branches/FreeTrainSDL/plugins/Chiname.RoadAccessory/src/RoadAccessory/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+#region LICENSE
+/*
+ * Copyright (C) 2007 - 2008 FreeTrain Team (http://freetrain.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#endregion LICENSE
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("FreeTrain road accessory plug-in")]
+[assembly: AssemblyDescription("accessories on road")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kohsuke Kawaguchi (kk@kohsuke.org)")]
+[assembly: AssemblyProduct("FreeTrain")]
+[assembly: AssemblyCopyright("(C)Copyright by Kohsuke Kawaguchi, 2003")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyDelaySign(false)]
+
+[assembly: AssemblyKeyName("")]
+[assembly: CLSCompliant(true)]
