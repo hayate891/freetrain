f804eeaff46f2452f51141e5d4dbe09e9bb5b026
jendave <jendave@bd9d7052-e9e4-4cea-aacc-3f037c7f4207>
2008-04-09 20:43:34 +0000

    icon problem in linux

--- branches/FreeTrainSDL/core/Framework/MainWindow.Designer.cs
+++ branches/FreeTrainSDL/core/Framework/MainWindow.Designer.cs
@@ -578,7 +578,6 @@ namespace FreeTrain.Framework
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.menuStrip);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip;
             this.Name = "MainWindow";
